02b49c3cc2a2db844a85d59c0f2ce9bdb01c370e
abhiram11 <abhiramsatpute@gmail.com>
2023-07-04 20:17:40 +0530

    added router logic and middleware, now write crud func

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CRUD-api-Go-Mux-Docker-Postgres-Docker-Compose
 CRUD api built using Go Lang, Mux, Postgres, Docker and Docker Compose
+Initial steps to install modules:
+
+1. go mod init api
+2. go get github.com/gorilla/mux
+3. go get github.com/lib/pq
--- main.go
+++ main.go
@@ -16,3 +16,36 @@ type User struct {
 	Name	string	`json:"name"`
 	Email	string `json:"email`
 }
+
+func main() {
+	//connect to database
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	//connection successful or no
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// this is called at the end of the main function, as a closing/cleanup function
+	defer db.Close()
+
+	//creater router to handle different requests
+	router := mux.NewRouter() //.StrictSlash(true)
+	router.HandleFunc("/users", getUsers(db)).Methods("GET")
+	router.HandleFunc("/users/{id}", getUser(db)).Methods("GET")
+	router.HandleFunc("/users", createUser(db)).Methods("POST")
+	router.HandleFunc("/users/{id}", updateUser(db)).Methods("PUT")
+	router.HandleFunc("/users/{id}", deleteUser(db)).Methods("DELETE")
+
+	// start server
+	log.Fatal(http.ListenAndServe(":8000", jsonContentTypeMiddleware(router) )) // hardcoded for now
+
+}
+
+func jsonContentTypeMiddleware ( next http.Handler) http.Handler {
+	return  http.HandlerFunc( func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	}
+}
+
+// defining functions/ controllers as fdefined in router.handleFuync
